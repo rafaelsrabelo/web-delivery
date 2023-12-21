0e0f1517ca4c69d7deb1c16e8c9f6b2897cbb894
Rafael Santana Rabelo <rafaelrabelo1610@gmail.com>
2023-12-21 12:15:43 -0300

    chore: :heavy_plus_sign: add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <>
-     <h1 className='text-3xl font-bold underline'>Hello</h1>
+      <h1 className="text-3xl font-bold underline">Hello</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
