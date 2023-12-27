62ebbddea63526bb6adc41a9430ae50a724cc660
Rafael Santana Rabelo <rafaelrabelo1610@gmail.com>
2023-12-27 12:44:02 -0300

    feat(orders/index.tsx): add support for updating order status with user information
    The changes in this commit add support for updating the order status with user information. Previously, the code only updated the status without considering the user. Now, the code checks if the selected order has a user associated with it. If not, it fetches the user information from the state and includes it in the updateOrderStatus action. This ensures that the order status is updated correctly with the user information.
    
    Additionally, the code now dispatches the fetchOrders action after updating the order status. This ensures that the orders list is refreshed with the latest data after the status update.
    
    The commit also includes error handling code to handle any errors that may occur during the status update process.

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -51,7 +51,7 @@ export function Orders() {
     selectedStatus: '',
   });
   const orderState = useSelector((state) => state.order);
-
+  const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
   const imageProfile = 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png';
@@ -81,15 +81,16 @@ export function Orders() {
       await dispatch(updateOrderStatus({ orderId: modalData.orderId, status: modalData.selectedStatus }));
 
       setOpen(false);
+
+      dispatch(fetchOrders(filterStatus));
+
+      const selectedOrder = orderState.orders.find((order) => order.id === modalData.orderId);
+      if (!selectedOrder.user && user) {
+        await dispatch(updateOrderStatus({ orderId: modalData.orderId, userId: user.id }));
+      }
     } catch (error) {
       console.error('Erro ao atualizar o status:', error.message);
-
-      // Adicionar tratamento específico para diferentes tipos de erro, se necessário
-      if (error.response) {
-        console.error('Erro na API:', error.response.data);
-      } else {
-        console.error('Erro desconhecido:', error);
-      }
+      // ... (restante do código de tratamento de erro)
     }
   };
 
