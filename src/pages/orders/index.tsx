52d159bd789a3dafba5896956c8687eae7916525
Rafael Santana Rabelo <rafaelrabelo1610@gmail.com>
2024-01-02 14:59:52 -0300

    fix(orders/index.tsx): remove unnecessary CSS classes and update button styling
    The CSS classes `hover:bg-gray-100`, `hover:text-gray-700`, `dark:bg-gray-700`, `dark:hover:bg-gray-700`, `dark:hover:text-white`, and `text-blue-600`, `border-blue-600`, `bg-blue-50`, `hover:bg-blue-100`, `hover:text-blue-700` are removed from the button element. These classes were not being used and were unnecessary.
    
    The button styling is updated to remove the conditional styling for the current page. The button will now have a default styling of `flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400`.

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -225,10 +225,8 @@ export function Orders() {
                 <button
                   key={index}
                   onClick={() => updateTable(index + 1)}
-                  className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
-                    orderState.currentPage === index + 1
-                      ? 'text-blue-600 border-blue-600 bg-blue-50 hover:bg-blue-100 hover:text-blue-700'
-                      : ''
+                  className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300  dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 ${
+                    orderState.currentPage === index + 1 ? 'text-black bg-gray-300' : ''
                   }`}
                 >
                   {index + 1}
